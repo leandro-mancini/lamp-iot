e942e9482ab97003e8af3bd5bfe554b05c7e5ed6
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2018-05-07 22:41:07 -0300

    Iniciando projeto Lamp com Johnny-five

---
+++ app.js
@@ -0,0 +1,18 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  this.repl.inject({
+    led: led
+  });
+  
+  setInterval(() => {
+    led.toggle();
+  }, 1000);
+
+});
